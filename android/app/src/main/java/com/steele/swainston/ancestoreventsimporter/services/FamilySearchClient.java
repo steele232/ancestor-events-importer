a9a14b94b0e7eba489806f3c216e8df94bcaa30c
coryswainston <swainston.cory89@gmail.com>
2018-10-28 21:45:12 -0600

    Initial project with basic UI

---
+++ android/app/src/main/java/com/steele/swainston/ancestoreventsimporter/services/FamilySearchClient.java
@@ -0,0 +1,10 @@
+package com.steele.swainston.ancestoreventsimporter.services;
+
+/**
+ * A client for accessing the FamilySearch API.
+ * This interface defines the API for the client.
+ */
+
+public interface FamilySearchClient {
+
+}
---
+++ android/app/src/main/java/com/steele/swainston/ancestoreventsimporter/services/FamilySearchHttpsClient.java
@@ -0,0 +1,9 @@
+package com.steele.swainston.ancestoreventsimporter.services;
+
+/**
+ * An implementation of FamilySearchClient that uses plain old
+ * HTTPS requests to retrieve data.
+ */
+
+public class FamilySearchHttpsClient implements FamilySearchClient {
+}
